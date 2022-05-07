cedc05c1871e18bed552b1884ae8a9292ebd01de
Igor Cavalera <66530386+Igor-Cavalera@users.noreply.github.com>
2022-05-06 23:55:42 -0300

    removendo-importacoes-todoItemJS

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,12 +1,10 @@
 import * as React from 'react';
-import { List } from '@mui/material';
 import { ListItem } from '@mui/material';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Checkbox from '@mui/material/Checkbox';
 import IconButton from '@mui/material/IconButton';
-import CommentIcon from '@mui/icons-material/Comment';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function TodoItem () {
